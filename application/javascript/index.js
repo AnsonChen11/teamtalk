1f9d5dd94d7d7986873d1dbe25bfb292949ae135
AnsonChen11 <ab67325@gmail.com>
2023-01-27 17:36:28 +0800

    feat: video meeting by webRTC, peerjs and socket io.

---
+++ application/javascript/index.js
@@ -0,0 +1,147 @@
+const socket = io("/");
+console.log("connect to socket")
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+const constraints = { 
+    video: { width:400, height:300 }, audio: true
+};
+myVideo.muted = true;
+
+let myPeer = new Peer({
+    path: "/peerjs",
+    host: "/",
+    port: "5000",
+    debug: 3
+});
+console.log("connect to peer")
+let myVideoStream;
+let roomId;
+let roomIdUrl = window.location.pathname;
+const peers = {}
+
+myPeer.on("open", userId => { //myPeer物件成功連接到伺服器時觸發，並會從伺服器端獲取一個唯一的 ID
+    roomId = roomIdUrl.match(/([^/]+)$/)[0];
+    socket.emit("joinRoom", roomId, userId);
+});
+    
+//瀏覽器使用 getUserMedia API 獲取使用者的視訊和音訊流
+navigator
+    .mediaDevices
+    .getUserMedia(constraints)
+    .then(stream => {
+        myVideoStream = stream;
+        addVideoStream(myVideo, stream);
+        console.log("獲得stream授權")
+        //新成員收到連接請求後，會發出 "call" 事件，並回答通話
+        myPeer.on("call", call => { //其他 myPeer物件向該myPeer發出呼叫時觸發。呼叫時，其他myPeer物件會建立一個call物件
+            console.log('someone call me');
+            call.answer(stream); //其他使用者發送自己的媒體流
+            console.log('call and answer');
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => { //註冊stream事件，當對方連接到Peer物件時並回答呼叫，對方會發送自己的媒體流，此時觸發stream事件
+                addVideoStream(video, userVideoStream) //加入其他使用者的stream
+                console.log("加入新媒體")
+            })
+        })
+        socket.on("userConnected", (userId) => {
+            console.log("user-connected", userId)
+            connectToNewUser(userId, stream);
+        })
+    })
+    .catch(error => {
+        console.log("Error accessing media devices.", error)
+    })
+
+
+function addVideoStream(video, userVideoStream){
+    try{
+        video.srcObject = userVideoStream;
+        video.controls = false;
+        video.playsinline = true;
+        video.addEventListener("loadedmetadata", () => {
+            video.play()
+        })
+        videoGrid.append(video)
+    }
+    catch(error){
+        console.log("Error adding video.", error)
+    }
+}
+
+
+//建立一個 myPeer.connect 到新成員的 myPeer。
+function connectToNewUser(userId, stream){
+    console.log('I call someone' + userId);
+    const call = myPeer.call(userId, stream); 
+    console.log(call)
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        console.log("收到對方接受回傳stream ")
+        addVideoStream(video, userVideoStream)
+    })
+
+    call.on("close", () => { //執行完peers[userId].close()才會移除使用者video
+        console.log("close on stream")
+        video.remove()
+    })
+    peers[userId] = call
+    console.log(peers, userId, call)
+}
+
+socket.on("userDisconnected", (userId) => {
+    try{
+        console.log(peers[userId])
+        if (peers[userId]) peers[userId].close() //關閉對該使用者的連接
+        console.log("斷開連線")
+    }
+    catch(e){
+        console.log('[error]','leave room :', e);
+        socket.emit('error','couldnt perform requested action');
+    }
+})
+
+const audioMute = document.getElementById("video__mute");
+const videoStop = document.getElementById("video__stop");
+const chatToggle = document.getElementById("video__chat");
+const sectionChat = document.getElementById("section__chat");
+const sectionVideo = document.querySelector(".section__video");
+const controlsVideo = document.querySelector(".controls__video");
+
+chatToggle.addEventListener("click", () => {
+    if(sectionChat.style.display === "none") {
+        sectionChat.style.display = "block";
+        sectionVideo.style.width = "80%";
+        controlsVideo.style.width = "80%";
+        sectionChat.style.width = "20%";
+    }else{
+        sectionChat.style.display = "none";
+        sectionVideo.style.width = "100%";
+        controlsVideo.style.width = "100%";
+    }
+});
+
+audioMute.addEventListener("click", () => {
+    const AudioTrack = myVideoStream.getAudioTracks()[0]; 
+    if(AudioTrack.enabled === true){
+        AudioTrack.enabled = false;
+        audioMute.style.backgroundColor = "rgb(192, 13, 13)";
+    }
+    else{
+        AudioTrack.enabled = true;
+        audioMute.style.backgroundColor = "rgb(82, 83, 83)";
+    }
+})
+
+videoStop.addEventListener("click", () => {
+    const videoTrack = myVideoStream.getVideoTracks()[0]; 
+    if(videoTrack.enabled === true){
+        videoTrack.enabled = false;
+        videoStop.style.backgroundColor = "rgb(192, 13, 13)";
+    }
+    else{
+        videoTrack.enabled = true;
+        videoStop.style.backgroundColor = "rgb(82, 83, 83)";
+    }
+})
+
+
--- server.js
+++ server.js
@@ -1,19 +1,66 @@
-const express = require('express')
+const express = require("express")
+const path = require("path");
 const hbs = require("hbs");
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
-const app = express()
+const http = require("http")
 const PORT = 5000
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true 
+});
+const { v4: uuidV4 } = require("uuid")
 
-// app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+
+app.use("/peerjs", peerServer);
+
+io.on("connection", socket => {
+    socket.on("joinRoom", (roomId, userId) => {
+        console.log(roomId, userId)
+        socket.join(roomId);
+        setTimeout(()=>{
+            socket.broadcast.to(roomId).emit("userConnected", userId);
+        }, 1000)
+        console.log("userConnected事件", userId)
+        
+        socket.on("sendMessage", (message) => {
+            socket.broadcast.to(roomId).emit("createMessage", message, userId);
+            console.log("createMessage事件", message, userId)
+        })
+
+        socket.on("disconnect", () => {
+            socket.leave(roomId);
+            setTimeout(()=>{
+                socket.broadcast.to(roomId).emit("userDisconnected", userId);
+            }, 1000)
+            console.log("userDisconnected事件", userId)
+        })
+
+        socket.on("chatMessage", (message) => {
+            console.log("收到訊息")
+            socket.broadcast.to(roomId).emit("createMessage", message, userId);
+            console.log("createMessage事件", message, userId)
+        })
+    })
+});
+
+
+//設定模板引擎
+app.engine("html", hbs.__express)
+//設定模板位置
+app.set("views", path.join(__dirname, "application", "views"))
+//設定靜態檔位置
+app.use(express.static(path.join(__dirname, "application")))
+//使用.render回傳html位置
 app.get('/', (req, res) => {
-res.send('hello world')
+    res.redirect(`/${uuidV4()}`)
 })
-app.listen(PORT, () => {
-console.log(`Express is listening on localhost:${PORT}`)
+app.get("/:room" , (req, res) => {
+    res.render("index.html", { roomId: req.params.room })
+});
+app.set("view engine", "hbs")
+
+server.listen(PORT, () => {
+    console.log(`Express is listening on localhost:${PORT}`)
 })
